e7058b1f2a57d2294d6910d2f4912b90df08c91b
Victor Clausen <s232604@dtu.dk>
2024-10-01 22:16:23 +0200

    en fed besked

--- main.c
+++ main.c
@@ -126,55 +126,97 @@ void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsi
 
 void cell_detection_function(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
   //Checks the whole image, by create a 12x12 area to check for white cells
-  
   for (int x = 0; x < BMP_WIDTH; x++){
     for (int y = 0; y < BMP_HEIGTH; y++){
-          int white = 0;
-          for (int i = 0; i <= 12; i++){
-            for (int j = 0; j <= 12; j++){
-              if (input_image[x + i][y + j][0] == 255){
-                white = 1;
-                break;
-              }
-            }
-            if (white == 1) break;
+      int white = 0;
+      for (int i = 0; i <= 12; i++){
+        for (int j = 0; j <= 12; j++){
+          if (input_image[x + i][y + j][0] == 255){
+            white = 1;
+            break;
           }
-          if (white == 0) continue;
+        }
+        if (white == 1) break;
+      }
+        if (white == 0) continue;
           int outer_white = 0;
 
           //Checks if there are any white cells on the outer layer
-          for (int i = -1; i <= 13; i++){
-            if (input_image[x+i][y-1][0]==255 || input_image[x+i][y+13][0]==255){
+        for (int i = -1; i <= 13; i++){
+          if (input_image[x+i][y-1][0]==255 || input_image[x+i][y+13][0]==255){
+            outer_white = 1;
+            break;
+          }
+          for (int j = -1; j <= 13; j++){
+            if(input_image[x-1][y+j][0]==255 || input_image[x+13][y+j][0]==255){
               outer_white = 1;
               break;
             }
-            for (int j = -1; j <= 13; j++){
-              if(input_image[x-1][y+j][0]==255 || input_image[x+13][y+j][0]==255){
-                outer_white = 1;
-                break;
-              }
-            }
           }
+        }
 
           //If there are no white cells in the outer layer, it adds 1 to cells and makes the 12x12 black
           //This also checks if coordinates are already in the array
-          if (outer_white == 0){
+        if (outer_white == 0){
+          int white_cell_exists = 0;
+          for (int k=0;k<=Max_celler;k++){
+            if (coordinate_x_cells[k]==x && coordinate_y_cells[k]==y){
+              white_cell_exists = 1;
+              break;
+            }
+          }
+          if (!white_cell_exists){
             cells++;
+            coordinate_x_cells[cells] = x+6;
+            coordinate_y_cells[cells] = y+6;
             for (int i = 0; i <= 12; i++){
               for (int j = 0; j <= 12; j++){
-                input_image[x + i][y + j][0] = 0;
-                for (int k=0;k<Max_celler;k++){
-                  if (!coordinate_x_cells[k]==x && !coordinate_y_cells[k]==y){
-                    coordinate_x_cells[cells] = x;
-                    coordinate_y_cells[cells] = y;
-                  }
-                }
+              input_image[x + i][y + j][0] = 0;
               }
             }
           }
+        }
     }
   }
 }
+
+void draw_crosses_on_image(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+    int cross_size = 7; // Size of the cross (half-length)
+    int color = 255;
+
+    // Copy input_image to output_image
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int y = 0; y < BMP_HEIGTH; y++) {
+            for (int c = 0; c < BMP_CHANNELS; c++) {
+                output_image[x][y][c] = input_image[x][y][c];
+            }
+        }
+    }
+
+    for (int i = 1; i < cells; i++) {
+        int x = coordinate_x_cells[i];
+        int y = coordinate_y_cells[i];
+
+        // Draw horizontal line of the cross
+        for (int j = -cross_size; j <= cross_size; j++) {
+            if (x + j >= 0 && x + j < BMP_WIDTH) {
+                output_image[x + j][y][0] = color;
+                output_image[x + j][y][1] = 0;
+                output_image[x + j][y][2] = 0;
+            }
+        }
+
+        // Draw vertical line of the cross
+        for (int j = -cross_size; j <= cross_size; j++) {
+            if (y + j >= 0 && y + j < BMP_HEIGTH) {
+                output_image[x][y + j][0] = color;
+                output_image[x][y + j][1] = 0;
+                output_image[x][y + j][2] = 0;
+            }
+        }
+    }
+}
+
 
 // Function to process all .bmp images in a folder
 
@@ -201,7 +243,6 @@ int main(int argc, char** argv)
       fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
       exit(1);
   }
-  int coordinate_x_cells[Max_celler], coordinate_y_cells[Max_celler];
   printf("Example program - 02132 - A1\n");
 
   // Load image from file
@@ -219,7 +260,10 @@ int main(int argc, char** argv)
   // Run binary erosion and cell detection
   binary_erosion(output_image2, output_image3);
 
+  //Draws the crosses
+  draw_crosses_on_image(input_image, output_image4);
   // Save image to file
+  
   write_bitmap(output_image4, argv[2]);
   printf("Done!\n");
   return 0;
